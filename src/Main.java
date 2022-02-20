8d6f56fb44af41941e01f2215323827780c479eb
Alexandreinfov <alexandrevalter88@gmail.com>
2022-02-20 09:20:31 -0300

    Create Class Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso em Java");
+        curso1.setDescricao("Descrição curso de java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso em js");
+        curso2.setDescricao("Descrição curso de js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrção Mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
