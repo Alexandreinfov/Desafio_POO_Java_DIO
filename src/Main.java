80855f575a40092cef712fa8a496547e0e607393
Alexandreinfov <alexandrevalter88@gmail.com>
2022-02-20 10:50:12 -0300

    Projeto final 20/02/2022

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,8 +22,49 @@ public class Main {
         mentoria.setDescricao("Descrção Mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+      /*  System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Devloper");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devAlexandre = new Dev();
+        devAlexandre.setNome("Alexandre");
+        devAlexandre.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos " +
+                devAlexandre.getNome()
+        + devAlexandre.getConteudosInscritos() + "\n");
+
+        devAlexandre.progredir();
+        System.out.println("Conteúdos Inscritos " +
+                devAlexandre.getNome()
+                + devAlexandre.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos " +
+                devAlexandre.getNome()
+                + devAlexandre.getConteudosConcluidos());
+
+        System.out.println("XP" + devAlexandre.calcularXp());
+
+        System.out.println("_________________");
+        Dev devPaulo = new Dev();
+        devPaulo.setNome("Paulo");
+        devPaulo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos " +
+                devPaulo.getNome()
+                + devPaulo.getConteudosInscritos() + "\n");
+       // devPaulo.progredir() ;
+        System.out.println("Conteúdos Inscritos " +
+                devPaulo.getNome()
+                + devPaulo.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos " +
+                devPaulo.getNome()
+                + devPaulo.getConteudosConcluidos());
+
+        System.out.println("XP" + devPaulo.calcularXp());
     }
 }
